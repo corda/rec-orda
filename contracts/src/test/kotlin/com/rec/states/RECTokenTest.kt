b7dbdde415e3888fa21e9323956efb4299061ea5
cyprienroche <cyprien.roche@r3.com>
2020-08-24 17:28:54 +0100

    [states] remove redundant tests in RECTokenTest

--- contracts/src/test/kotlin/com/rec/states/RECTokenTest.kt
+++ contracts/src/test/kotlin/com/rec/states/RECTokenTest.kt
@@ -16,38 +16,4 @@ class RECTokenTest {
         val token = RECToken(alice, bob, 0)
         assertEquals(0, token.quantity)
     }
-
-    // These tests are not necessary as this is the same as testing the `data class` construct.
-    @Test
-    fun `equals and hashcode identifies identical instances`() {
-        val token1 = RECToken(alice, bob, 2)
-        val token2 = RECToken(alice, bob, 2)
-
-        assertEquals(token1, token2)
-        assertEquals(token1.hashCode(), token2.hashCode())
-    }
-
-    @Test
-    fun `equals and hashcode differentiate by issuer`() {
-        val token1 = RECToken(alice, bob, 2)
-        val token2 = RECToken(carly, bob, 2)
-        Assert.assertNotEquals(token1, token2)
-        Assert.assertNotEquals(token1.hashCode().toLong(), token2.hashCode().toLong())
-    }
-
-    @Test
-    fun `equals and hashcode differentiate by owner`() {
-        val token1 = RECToken(alice, bob, 2)
-        val token2 = RECToken(alice, carly, 2)
-        Assert.assertNotEquals(token1, token2)
-        Assert.assertNotEquals(token1.hashCode().toLong(), token2.hashCode().toLong())
-    }
-
-    @Test
-    fun `equals and hashcode differentiate by amount`() {
-        val token1 = RECToken(alice, bob, 2)
-        val token2 = RECToken(alice, bob, 3)
-        Assert.assertNotEquals(token1, token2)
-        Assert.assertNotEquals(token1.hashCode().toLong(), token2.hashCode().toLong())
-    }
 }
