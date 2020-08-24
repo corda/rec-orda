5a1fcd3826340334ebf921c0b8aeed607c169be7
cyprienroche <cyprien.roche@r3.com>
2020-08-24 17:51:07 +0100

    use AbstractParty as it is needed with FungibleTokens in flows

--- contracts/src/main/kotlin/com/rec/states/RECToken.kt
+++ contracts/src/main/kotlin/com/rec/states/RECToken.kt
@@ -3,11 +3,15 @@ package com.rec.states
 import com.r3.corda.lib.tokens.contracts.states.FungibleToken
 import com.r3.corda.lib.tokens.contracts.utilities.issuedBy
 import com.r3.corda.lib.tokens.contracts.utilities.of
+import com.rec.contracts.RECTokenContract
+import net.corda.core.contracts.BelongsToContract
+import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
 
+@BelongsToContract(RECTokenContract::class)
 data class RECToken(
         override val issuer: Party,
-        override val holder: Party,
+        override val holder: AbstractParty,
         val quantity: Long
 //        val issuance: Issuance,
 //        val uid: Long
