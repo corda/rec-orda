cd9670a19bf70736f9f46e8a230213e9f8754775
cyprienroche <cyprien.roche@r3.com>
2020-08-25 10:45:42 +0100

    [states] add EnergySource val to RECTokenType

--- contracts/src/main/kotlin/com/rec/states/RECTokenType.kt
+++ contracts/src/main/kotlin/com/rec/states/RECTokenType.kt
@@ -4,9 +4,9 @@ import com.r3.corda.lib.tokens.contracts.types.TokenType
 import com.r3.corda.lib.tokens.contracts.utilities.getAttachmentIdForGenericParam
 import net.corda.core.crypto.SecureHash
 
-class RECTokenType : TokenType(IDENTIFIER, FRACTION_DIGITS) {
+class RECTokenType(val source: EnergySource) : TokenType(IDENTIFIER, FRACTION_DIGITS) {
     companion object {
-        val contractAttachment: SecureHash = RECTokenType().getAttachmentIdForGenericParam()!!
+        val contractAttachment: SecureHash = RECTokenType(EnergySource.WIND).getAttachmentIdForGenericParam()!!
         const val IDENTIFIER = "REC"
         const val FRACTION_DIGITS = 0
     }
