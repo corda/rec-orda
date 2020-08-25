aa6b10bad6f1f31d80102c151654afe54603713f
cyprienroche <cyprien.roche@r3.com>
2020-08-25 09:27:41 +0100

    [states] add two tests for RECTokenTest

--- contracts/src/test/kotlin/com/rec/states/RECTokenTest.kt
+++ contracts/src/test/kotlin/com/rec/states/RECTokenTest.kt
@@ -1,5 +1,6 @@
 package com.rec.states
 
+import com.r3.corda.lib.tokens.contracts.states.FungibleToken
 import net.corda.core.identity.CordaX500Name
 import net.corda.testing.core.TestIdentity
 import org.junit.Test
@@ -8,11 +9,22 @@ import kotlin.test.assertEquals
 class RECTokenTest {
     private val alice = TestIdentity(CordaX500Name("Alice", "London", "GB")).party
     private val bob = TestIdentity(CordaX500Name("Bob", "London", "GB")).party
-    private val carly = TestIdentity(CordaX500Name("Carly", "London", "GB")).party
 
     @Test
     fun `it accepts a 0 amount`() {
-        val token = RECToken(alice, bob, 0)
-        assertEquals(0, token.quantity)
+        val token: FungibleToken = RECToken(alice, bob, 0)
+        assertEquals(0, token.amount.quantity)
+    }
+
+    @Test
+    fun `token type used is RECTokenType`() {
+        val token: FungibleToken = RECToken(alice, bob, 0)
+        assertEquals(RECTokenType(), token.tokenType)
+    }
+
+    @Test
+    fun `holder is the only participant`() {
+        val token: FungibleToken = RECToken(alice, bob, 0)
+        assertEquals(bob, token.participants.single())
     }
 }
