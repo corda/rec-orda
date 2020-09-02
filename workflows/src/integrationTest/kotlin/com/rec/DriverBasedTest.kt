c824707cb8f175a38173eee2d449441aa5c12908
cyprienroche <cyprien.roche@r3.com>
2020-09-02 11:15:29 +0100

    [integrationTest] refactor issue flow and testing in separate method and allow alice to move and bob to redeem in user permissions

--- workflows/src/integrationTest/kotlin/com/rec/DriverBasedTest.kt
+++ workflows/src/integrationTest/kotlin/com/rec/DriverBasedTest.kt
@@ -6,11 +6,14 @@ import com.rec.DriverTestHelpers.Companion.withDriver
 import com.rec.DriverTestHelpers.Companion.withUsers
 import com.rec.flows.IssueFlows
 import com.rec.flows.MoveFlows
+import com.rec.flows.RedeemFlows
 import com.rec.states.EnergySource
 import com.rec.states.FungibleRECToken
 import com.rec.states.RECToken
+import net.corda.core.contracts.StateAndRef
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.messaging.CordaRPCOps
+import net.corda.core.messaging.vaultQueryBy
 import net.corda.core.messaging.vaultTrackBy
 import net.corda.core.node.services.Vault
 import net.corda.core.utilities.getOrThrow
@@ -19,6 +22,7 @@ import net.corda.testing.core.TestIdentity
 import net.corda.testing.core.expect
 import net.corda.testing.core.expectEvents
 import net.corda.testing.core.singleIdentity
+import net.corda.testing.driver.NodeHandle
 import net.corda.testing.node.User
 import org.junit.Test
 import rx.Observable
@@ -30,16 +34,18 @@ class DriverBasedTest {
 
     private val aliceUser = User("aliceUser", "testPassword1", permissions = setOf(
             Permissions.startFlow<IssueFlows.Initiator>(),
+            Permissions.startFlow<MoveFlows.Initiator>(),
             Permissions.invokeRpc("vaultTrackBy")
     ))
 
     private val bobUser = User("bobUser", "testPassword2", permissions = setOf(
             Permissions.startFlow<MoveFlows.Initiator>(),
+            Permissions.startFlow<RedeemFlows.Initiator>(),
             Permissions.invokeRpc("vaultTrackBy")
     ))
 
     @Test
-    fun `issue, move and redeem test`() = withDriver {
+    fun `issue, move and redeem integration test`() = withDriver {
 
         val (alice, bob) = startNodes(
                 prosumer withUsers listOf(aliceUser),
@@ -49,23 +55,29 @@ class DriverBasedTest {
         val aliceProxy: CordaRPCOps = alice.getProxy("aliceUser", "testPassword1")
         val bobProxy: CordaRPCOps = bob.getProxy("bobUser", "testPassword2")
 
-        val bobVaultUpdates: Observable<Vault.Update<FungibleRECToken>> = bobProxy.vaultTrackBy<FungibleRECToken>().updates
+        val aliceVaultUpdates: Observable<Vault.Update<FungibleRECToken>> = aliceProxy.vaultTrackBy<FungibleRECToken>().updates
 
+        issue(aliceProxy, alice, aliceVaultUpdates)
+
+
+    }
+
+    private fun issue(aliceProxy: CordaRPCOps, alice: NodeHandle, aliceVaultUpdates: Observable<Vault.Update<FungibleRECToken>>) {
         aliceProxy.startFlowDynamic(
                 IssueFlows.Initiator::class.java,
-                bob.nodeInfo.singleIdentity(),
+                alice.nodeInfo.singleIdentity(),
                 10L,
                 EnergySource.WIND
         ).returnValue.getOrThrow()
 
-        bobVaultUpdates.expectEvents {
+        aliceVaultUpdates.expectEvents {
             expect { update ->
-                println("Bob got vault update of $update")
                 val token: FungibleRECToken = update.produced.first().state.data
                 assertEquals(RECToken(EnergySource.WIND), token.recToken)
                 assertEquals(10L, token.amount.quantity)
             }
         }
-
     }
+
+
 }
