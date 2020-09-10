ecc500f604d8ed1c6b8cefa5988a30647c6158a2
cyprienroche <cyprien.roche@r3.com>
2020-09-10 16:24:57 +0100

    [fungibleToken] use FungibleToken subFlows instead of FungibleRECToken Move and Redeem Tokens flows

--- workflows/src/main/kotlin/com/rec/flows/MoveFlows.kt
+++ workflows/src/main/kotlin/com/rec/flows/MoveFlows.kt
@@ -1,70 +1,22 @@
 package com.rec.flows
 
 import co.paralleluniverse.fibers.Suspendable
-import com.r3.corda.lib.tokens.workflows.flows.move.MoveTokensFlow
-import com.r3.corda.lib.tokens.workflows.flows.move.MoveTokensFlowHandler
-import com.r3.corda.lib.tokens.workflows.flows.rpc.MoveFungibleTokensHandler
-import com.r3.corda.lib.tokens.workflows.utilities.sessionsForParties
-import com.rec.states.FungibleRECToken
-import net.corda.core.contracts.StateAndRef
-import net.corda.core.flows.*
-import net.corda.core.identity.AbstractParty
+import com.r3.corda.lib.tokens.contracts.types.TokenType
+import com.r3.corda.lib.tokens.workflows.flows.rpc.MoveFungibleTokens
+import com.r3.corda.lib.tokens.workflows.types.PartyAndAmount
+import net.corda.core.flows.FlowException
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.InitiatingFlow
+import net.corda.core.flows.StartableByRPC
 import net.corda.core.transactions.SignedTransaction
-import net.corda.core.utilities.ProgressTracker
 
 object MoveFlows {
-    /**
-     * Started by a [FungibleRECToken.holder] to move multiple states where it is the only holder.
-     * It is an [InitiatingFlow] flow and its counterpart, which already exists, is
-     * [MoveFungibleTokensHandler], while not being automatically [InitiatedBy] it.
-     * This constructor would be called by RPC or by [FlowLogic.subFlow]. In particular one that, given sums,
-     * fetches states in the vault.
-     */
     @InitiatingFlow
     @StartableByRPC
-    class Initiator(
-            private val inputTokens: List<StateAndRef<FungibleRECToken>>,
-            private val outputTokens: List<FungibleRECToken>,
-            override val progressTracker: ProgressTracker = tracker()
-    ) : FlowLogic<SignedTransaction?>() {
-
-        private val holder: AbstractParty
-
-        init {
-            val holders = inputTokens.map { it.state.data.holder }.distinct()
-            if (holders.size != 1) throw IllegalArgumentException("There can be only one holder")
-            holder = holders.single()
-        }
-
+    class Initiator(private val partiesAndAmounts: List<PartyAndAmount<TokenType>>) : FlowLogic<SignedTransaction?>() {
 
         @Suspendable
         @Throws(FlowException::class)
-        override fun call(): SignedTransaction {
-            progressTracker.currentStep = PREPARING_TO_PASS_ON
-            // We don't want to sign transactions where our signature is not needed.
-            if (holder != ourIdentity) throw FlowException("I must be a holder.")
-
-            val allParticipants = outputTokens.map { it.holder }.distinct() + holder
-
-            val participantSessions = this.sessionsForParties(allParticipants)
-
-            return subFlow(MoveTokensFlow(inputTokens, outputTokens, participantSessions))
-        }
-
-        companion object {
-            private val PREPARING_TO_PASS_ON = ProgressTracker.Step("Preparing to pass on to Tokens move flow.")
-            private val PASSING_TO_SUB_MOVE: ProgressTracker.Step = ProgressTracker.Step("Passing on to Tokens move flow.")
-
-            fun tracker(): ProgressTracker {
-                return ProgressTracker(PREPARING_TO_PASS_ON, PASSING_TO_SUB_MOVE)
-            }
-        }
-    }
-
-    @InitiatedBy(Initiator::class)
-    class Responder(private val otherSession: FlowSession) : FlowLogic<Unit>() {
-        @Suspendable
-        @Throws(FlowException::class)
-        override fun call(): Unit = subFlow(MoveTokensFlowHandler(otherSession))
+        override fun call(): SignedTransaction = subFlow(MoveFungibleTokens(partiesAndAmounts))
     }
 }
--- workflows/src/main/kotlin/com/rec/flows/RedeemFlows.kt
+++ workflows/src/main/kotlin/com/rec/flows/RedeemFlows.kt
@@ -1,68 +1,24 @@
 package com.rec.flows
 
 import co.paralleluniverse.fibers.Suspendable
-import com.r3.corda.lib.tokens.workflows.flows.move.MoveTokensFlowHandler
+import com.r3.corda.lib.tokens.contracts.states.FungibleToken
+import com.r3.corda.lib.tokens.contracts.types.TokenType
 import com.r3.corda.lib.tokens.workflows.flows.redeem.RedeemTokensFlow
 import com.r3.corda.lib.tokens.workflows.flows.redeem.RedeemTokensFlowHandler
-import com.rec.states.FungibleRECToken
+import com.r3.corda.lib.tokens.workflows.flows.rpc.RedeemFungibleTokens
+import net.corda.core.contracts.Amount
 import net.corda.core.contracts.StateAndRef
 import net.corda.core.flows.*
+import net.corda.core.identity.Party
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.utilities.ProgressTracker
 
 object RedeemFlows {
-
-
-    /**
-     * Started by a $[FungibleRECToken.holder] to redeem multiple states of 1 issuer where it is one of the holders.
-     * It is not $[InitiatingFlow].
-     * This constructor would be called by RPC or by $[FlowLogic.subFlow].
-     */
-
-    // By requiring an exact list of states, this flow assures absolute precision at the expense of
-    // user-friendliness.
     @InitiatingFlow
     @StartableByRPC
-    class Initiator(
-            private val inputTokens: List<StateAndRef<FungibleRECToken>>,
-            override val progressTracker: ProgressTracker = tracker()
-    ) : FlowLogic<SignedTransaction>() {
-
+    class Initiator(private val amount: Amount<TokenType>, private val issuer: Party) : FlowLogic<SignedTransaction>() {
         @Suspendable
         @Throws(FlowException::class)
-        override fun call(): SignedTransaction {
-            progressTracker.currentStep = PREPARING_TO_PASS_ON
-            val allIssuers = inputTokens
-              .map { it.state.data.issuer }.distinct()
-            // Remove duplicates as it would be an issue when initiating flows, at least.
-            // We don't want to sign transactions where our signature is not needed.
-
-            if (allIssuers.isEmpty()) throw FlowException("Cannot have empty input.")
-            if (allIssuers.size > 1) throw FlowException("Must specify at most one issuer to redeem.")
-
-            val issuer = allIssuers.single()
-
-            val issuerSession = initiateFlow(issuer)
-
-            progressTracker.currentStep = PASSING_TO_SUB_REDEEM
-
-            return subFlow(RedeemTokensFlow(inputTokens, null, issuerSession))
-        }
-
-        companion object {
-            private val PREPARING_TO_PASS_ON = ProgressTracker.Step("Preparing to pass on to Tokens redeem flow.")
-            private val PASSING_TO_SUB_REDEEM = ProgressTracker.Step("Passing on to Tokens redeem flow.")
-            fun tracker(): ProgressTracker {
-                return ProgressTracker(PREPARING_TO_PASS_ON, PASSING_TO_SUB_REDEEM)
-            }
-        }
+        override fun call(): SignedTransaction = subFlow(RedeemFungibleTokens(amount, issuer))
     }
-
-    @InitiatedBy(RedeemFlows.Initiator::class)
-    class Responder(private val otherSession: FlowSession) : FlowLogic<Unit>() {
-        @Suspendable
-        @Throws(FlowException::class)
-        override fun call(): Unit = subFlow(RedeemTokensFlowHandler(otherSession))
-    }
-
 }
